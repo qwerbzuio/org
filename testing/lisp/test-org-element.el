6b657ace2c5e36d4d6f71d87b1170f189912112d
Benjamin Motz <benjamin.motz@mailbox.org>
2019-05-28 13:37:39 +0200

    [inlinetask] Don't consider trailing blank lines part of inlinetask
    This would glue together paragraphs on exporting, which is not desired:
    Some text.
    *************** Some task
    *************** END
    
    Next paragraph.
    
    Exports to
    Some text.
    Next paragraph.
    instead of
    Some text.
    
    Next paragraph.

--- testing/lisp/test-org-element.el
+++ testing/lisp/test-org-element.el
@@ -1387,7 +1387,10 @@ DEADLINE: <2012-03-29 thu.>"
       ;; Handle non-empty blank line at the end of buffer.
       (should
        (org-test-with-temp-text "*************** Task\n*************** END\n "
-	 (= (org-element-property :end (org-element-at-point)) (point-max)))))))
+	 (= (org-element-property :end (org-element-at-point))
+	    (save-excursion
+	      (goto-char (point-max))
+	      (line-beginning-position))))))))
 
 
 ;;;; Italic
