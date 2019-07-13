8b4f64072f3a17c651208bca72bf2298e7b6e7c0
Benjamin Motz <benjamin.motz@mailbox.org>
2019-07-13 12:50:34 +0200

    [inlinetask] Test export of subsequent inlinetasks
    This fails in a more complex example; however, I couldn't reproduce it
    here, yet.

--- testing/lisp/test-ox.el
+++ testing/lisp/test-ox.el
@@ -2746,7 +2746,49 @@ Para2"
 		  org-export-filter-final-output-functions)
 	      (org-test-with-temp-text "*** TODO Inline"
 		(org-export-as (org-test-default-backend)
-			       nil nil nil '(:with-tasks nil))))))))
+			       nil nil nil '(:with-tasks nil))))))
+    ;; Export subsequent inlinetasks to HTML
+    (should
+     (equal
+      "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
+\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">
+<head>
+<!--  -->
+<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />
+<title>&lrm;</title>
+<meta name=\"generator\" content=\"Org mode\" />
+</head>
+<body>
+<div id=\"content\">
+<div class=\"inlinetask\">
+<b>Inline1</b><br />
+</div>
+<p>
+
+</p>
+
+<div class=\"inlinetask\">
+<b>Inline2</b><br />
+</div>
+</div>
+</body>
+</html>"
+      (let ((org-inlinetask-min-level 3)
+	    (org-export-with-author nil)
+	    (org-export-with-date nil)
+	    (org-export-with-timestamps nil)
+	    (org-export-with-toc nil)
+	    (org-export-with-section-numbers nil)
+	    (org-html-head-include-default-style nil)
+	    (org-html-head-include-scripts nil)
+	    (org-html-postamble nil)
+	    (org-html-metadata-timestamp-format ""))
+	(org-test-with-temp-text
+	    "*** Inline1\n*** END\n\n*** Inline2\n*** END"
+	  (org-export-as 'html)))))))
 
 
 
